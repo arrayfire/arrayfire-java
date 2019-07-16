00a91f2471fe95b8c8b024d89ace67e7793bbae2
Shady Boukhary <sb199898.sb@gmail.com>
2019-07-16 17:19:07 -0400

    Moved JavaExceptions and related functions to java namespace.

---
+++ src/java/ThrownJavaException.h
@@ -0,0 +1,17 @@
+#include <arrayfire.h>
+#include <assert.h>
+#include <jni.h>
+#include <stdexcept>
+
+namespace java {
+class ThrownJavaException : std::runtime_error {
+ public:
+  ThrownJavaException() : std::runtime_error("") {}
+  ThrownJavaException(const std::string &msg) : std::runtime_error(msg) {}
+
+  static inline void assert_no_exception(JNIEnv *env) {
+    if (env->ExceptionCheck() == JNI_TRUE)
+      throw ThrownJavaException("assert_no_exception");
+  }
+};
+}  // namespace java
