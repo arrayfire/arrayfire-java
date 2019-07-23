2097d02b2424c9c0c3fd72240a6381f00eb52e94
Shady Boukhary <sb199898.sb@gmail.com>
2019-07-23 14:28:10 -0400

    Added statistics function: af_var_*

--- com/arrayfire/Statistics.java
+++ com/arrayfire/Statistics.java
@@ -17,6 +17,22 @@ public class Statistics extends ArrayFire {
 
   static private native DoubleComplex afMeanAllDoubleComplexWeighted(long ref, long weightsRef);
 
+  static private native long afVar(long ref, boolean isBiased, int dim);
+
+  static private native long afVarWeighted(long ref, long weightsRef, int dim);
+
+  static private native double afVarAll(long ref, boolean isBiased);
+
+  static private native double afVarAllWeighted(long ref, long weightsRef);
+
+  static private native FloatComplex afVarAllFloatComplex(long ref, boolean isBiased);
+
+  static private native DoubleComplex afVarAllDoubleComplex(long ref, boolean isBiased);
+
+  static private native FloatComplex afVarAllFloatComplexWeighted(long ref, long weightsRef);
+
+  static private native DoubleComplex afVarAllDoubleComplexWeighted(long ref, long weightsRef);
+
   static public Array mean(final Array in, int dim) {
     return new Array(afMean(in.ref, dim));
   }
@@ -56,6 +72,55 @@ public class Statistics extends ArrayFire {
     }
 
     double res = afMeanAllWeighted(in.ref, weights.ref);
+    if (type == Float.class) {
+      return type.cast(Float.valueOf((float) res));
+    } else if (type == Double.class) {
+      return type.cast(Double.valueOf((double) res));
+    } else if (type == Integer.class) {
+      return type.cast(Integer.valueOf((int) res));
+    }
+    throw new Exception("Unknown type");
+  }
+
+  static public Array var(final Array in, boolean isBiased, int dim) {
+    return new Array(afVar(in.ref, isBiased, dim));
+  }
+
+  static public Array var(final Array in, final Array weights, int dim) {
+    return new Array(afVarWeighted(in.ref, weights.ref, dim));
+  }
+
+  static public <T> T var(final Array in, boolean isBiased, Class<T> type) throws Exception {
+    if (type == FloatComplex.class) {
+      FloatComplex res = (FloatComplex) afVarAllFloatComplex(in.ref, isBiased);
+      return type.cast(res);
+    } else if (type == DoubleComplex.class) {
+      DoubleComplex res = (DoubleComplex) afVarAllDoubleComplex(in.ref, isBiased);
+      return type.cast(res);
+    }
+
+    double res = afVarAll(in.ref, isBiased);
+    if (type == Float.class) {
+      return type.cast(Float.valueOf((float) res));
+    } else if (type == Double.class) {
+      return type.cast(Double.valueOf((double) res));
+    } else if (type == Integer.class) {
+      return type.cast(Integer.valueOf((int) res));
+    }
+    throw new Exception("Unknown type");
+  }
+
+  static public <T> T var(final Array in, final Array weights, Class<T> type) throws Exception {
+    if (type == FloatComplex.class) {
+      FloatComplex res = (FloatComplex) afVarAllFloatComplexWeighted(in.ref, weights.ref);
+      return type.cast(res);
+    } else if (type == DoubleComplex.class) {
+      System.out.println(Long.toString(weights.ref));
+      DoubleComplex res = (DoubleComplex) afVarAllDoubleComplexWeighted(in.ref, weights.ref);
+      return type.cast(res);
+    }
+
+    double res = afVarAllWeighted(in.ref, weights.ref);
     if (type == Float.class) {
       return type.cast(Float.valueOf((float) res));
     } else if (type == Double.class) {
--- examples/HelloWorld.java
+++ examples/HelloWorld.java
@@ -8,72 +8,71 @@ public class HelloWorld {
     Array a = new Array(), b = new Array(), c = new Array(), d = new Array();
     Array f = new Array();
     try {
-        Util.info();
-        System.out.println("Create a 5-by-3 matrix of random floats on the GPU");
-        Data.randu(a, new int[] {5, 3}, Array.FloatType);
-        a.print("a");
+      Util.info();
+      System.out.println("Create a 5-by-3 matrix of random floats on the GPU");
+      Data.randu(a, new int[] { 5, 3 }, Array.FloatType);
+      a.print("a");
 
-        System.out.println("Element-wise arithmetic");
-        Arith.sin(b, a);
-        b.print("b");
+      System.out.println("Element-wise arithmetic");
+      Arith.sin(b, a);
+      b.print("b");
 
-        System.out.println("Fourier transform the result");
-        Signal.fft(c, b);
-        c.print("c");
+      System.out.println("Fourier transform the result");
+      Signal.fft(c, b);
+      c.print("c");
 
-        System.out.println("Matmul b and c");
-        Arith.mul(d, b, c);
-        d.print("d");
+      System.out.println("Matmul b and c");
+      Arith.mul(d, b, c);
+      d.print("d");
 
+      System.out.println("Calculate weighted variance.");
+      Array forVar = new Array();
+      Array weights = new Array();
+      Data.randn(forVar, new int[] { 5, 3 }, Array.DoubleType);
+      Data.randn(weights, new int[] { 5, 3 }, Array.DoubleType);
+      forVar.print("forVar");
 
-        Array forMean = new Array();
-        Array weights = new Array();
-        Data.randn(forMean, new int[] {3, 3}, Array.FloatComplexType);
-        Data.randn(weights, new int[] {3, 3}, Array.FloatType);
-        forMean.print("forMean");
+      double abc = Statistics.mean(forVar, weights, Double.class);
+      System.out.println(String.format("Variance is: %f", abc));
 
-        FloatComplex abc = Statistics.mean(forMean, weights, FloatComplex.class);
-        System.out.println(String.format("Mean is: %f and %f", abc.real(), abc.imag()));
+      System.out.println("Create a 2-by-3 matrix from host data");
+      int[] dims = new int[] { 2, 3 };
+      int total = 1;
+      for (int dim : dims) {
+        total *= dim;
+      }
+      float[] data = new float[total];
+      Random rand = new Random();
 
-        System.out.println("Create a 2-by-3 matrix from host data");
-        int[] dims = new int[] { 2, 3 };
-        int total = 1;
-        for (int dim : dims) {
-          total *= dim;
+      for (int i = 0; i < total; i++) {
+        double tmp = Math.ceil(rand.nextDouble() * 10) / 10;
+        data[i] = (float) (tmp);
+      }
+      Array e = new Array(dims, data);
+      e.print("e");
+
+      System.out.println("Add e and random array");
+      Array randa = new Array();
+      Data.randu(randa, dims, Array.FloatType);
+      Arith.add(f, e, randa);
+      f.print("f");
+
+      System.out.println("Copy result back to host.");
+      float[] result = f.getFloatArray();
+      for (int i = 0; i < dims[0]; i++) {
+        for (int y = 0; y < dims[1]; y++) {
+          System.out.print(result[y * dims[0] + i] + " ");
         }
-        float[] data = new float[total];
-        Random rand = new Random();
-
-        for (int i = 0; i < total; i++) {
-            double tmp = Math.ceil(rand.nextDouble() * 10) / 10;
-            data[i] = (float) (tmp);
-        }
-        Array e = new Array(dims, data);
-        e.print("e");
-
-        System.out.println("Add e and random array");
-        Array randa = new Array();
-        Data.randu(randa, dims, Array.FloatType);
-        Arith.add(f, e, randa);
-        f.print("f");
-
-
-        System.out.println("Copy result back to host.");
-        float[] result = f.getFloatArray();
-        for (int i = 0; i < dims[0]; i++) {
-            for (int y = 0; y < dims[1]; y++) {
-                System.out.print(result[y * dims[0] + i] + " ");
-            }
-            System.out.println();
-        }
-        a.close();
-        b.close();
-        c.close();
-        d.close();
-        e.close();
-        f.close();
+        System.out.println();
+      }
+      a.close();
+      b.close();
+      c.close();
+      d.close();
+      e.close();
+      f.close();
     } catch (Exception ex) {
-        ex.printStackTrace();
+      ex.printStackTrace();
     }
   }
 }
