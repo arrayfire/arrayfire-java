1f4cd41ad7e84f30091d0aaf5b684ecc210eadce
Shady Boukhary <sb199898.sb@gmail.com>
2019-07-23 13:11:13 -0400

    Added complex types to mean wrappers and weighted mean.

--- com/arrayfire/Statistics.java
+++ com/arrayfire/Statistics.java
@@ -1,43 +1,68 @@
 package com.arrayfire;
 
 public class Statistics extends ArrayFire {
-    static private native long afMean(long ref, int dim);
+  static private native long afMean(long ref, int dim);
 
-    static private native long afMeanWeighted(long ref, long weightsRef, int dim);
+  static private native long afMeanWeighted(long ref, long weightsRef, int dim);
 
-    static private native double afMeanAll(long ref, int[] dims);
+  static private native double afMeanAll(long ref);
 
-    static private native FloatComplex afMeanAllFloatComplex(long ref, int[] dims);
-    static private native DoubleComplex afMeanAllDoubleComplex(long ref, int[] dims);
+  static private native double afMeanAllWeighted(long ref, long weightsRef);
 
-    //static private native jlong afMeanAllWeighted(long ref, long weightsRef);
+  static private native FloatComplex afMeanAllFloatComplex(long ref);
 
-    static public Array mean(final Array in, int dim) {
-        return new Array(afMean(in.ref, dim));
+  static private native DoubleComplex afMeanAllDoubleComplex(long ref);
+
+  static private native FloatComplex afMeanAllFloatComplexWeighted(long ref, long weightsRef);
+
+  static private native DoubleComplex afMeanAllDoubleComplexWeighted(long ref, long weightsRef);
+
+  static public Array mean(final Array in, int dim) {
+    return new Array(afMean(in.ref, dim));
+  }
+
+  static public Array mean(final Array in, final Array weights, int dim) {
+    return new Array(afMeanWeighted(in.ref, weights.ref, dim));
+  }
+
+  static public <T> T mean(final Array in, Class<T> type) throws Exception {
+    if (type == FloatComplex.class) {
+      FloatComplex res = (FloatComplex) afMeanAllFloatComplex(in.ref);
+      return type.cast(res);
+    } else if (type == DoubleComplex.class) {
+      DoubleComplex res = (DoubleComplex) afMeanAllDoubleComplex(in.ref);
+      return type.cast(res);
     }
 
-    static public Array mean(final Array in, final Array weights, int dim) {
-        return new Array(afMeanWeighted(in.ref, weights.ref, dim));
+    double res = afMeanAll(in.ref);
+    if (type == Float.class) {
+      return type.cast(Float.valueOf((float) res));
+    } else if (type == Double.class) {
+      return type.cast(Double.valueOf((double) res));
+    } else if (type == Integer.class) {
+      return type.cast(Integer.valueOf((int) res));
+    }
+    throw new Exception("Unknown type");
+  }
+
+  static public <T> T mean(final Array in, final Array weights, Class<T> type) throws Exception {
+    if (type == FloatComplex.class) {
+      FloatComplex res = (FloatComplex) afMeanAllFloatComplexWeighted(in.ref, weights.ref);
+      return type.cast(res);
+    } else if (type == DoubleComplex.class) {
+      System.out.println(Long.toString(weights.ref));
+      DoubleComplex res = (DoubleComplex) afMeanAllDoubleComplexWeighted(in.ref, weights.ref);
+      return type.cast(res);
     }
 
-    static public <T> T mean(final Array in, Class<T> type) throws Exception {
-        if (type == FloatComplex.class) {
-            FloatComplex res = (FloatComplex)afMeanAllFloatComplex(in.ref, in.dims());
-            return type.cast(res);
-        } else if (type == DoubleComplex.class) {
-            DoubleComplex res = (DoubleComplex)afMeanAllDoubleComplex(in.ref, in.dims());
-            return type.cast(res);
-        }
-
-        double res = afMeanAll(in.ref, in.dims());
-        if (type == Float.class) {
-            return type.cast(Float.valueOf((float)res));
-        } else if (type == Double.class) {
-            return type.cast(Double.valueOf((double) res));
-        } else if (type == Integer.class) {
-            return type.cast(Integer.valueOf((int) res));
-        }
-        throw new Exception("Unknown type");
+    double res = afMeanAllWeighted(in.ref, weights.ref);
+    if (type == Float.class) {
+      return type.cast(Float.valueOf((float) res));
+    } else if (type == Double.class) {
+      return type.cast(Double.valueOf((double) res));
+    } else if (type == Integer.class) {
+      return type.cast(Integer.valueOf((int) res));
     }
+    throw new Exception("Unknown type");
+  }
 }
-
--- src/statistics.cpp
+++ src/statistics.cpp
@@ -6,49 +6,63 @@ BEGIN_EXTERN_C
 #define STATISTICS_FUNC(FUNC) AF_MANGLE(Statistics, FUNC)
 
 JNIEXPORT jlong JNICALL STATISTICS_FUNC(afMean)(JNIEnv *env, jclass clazz,
-                                               jlong ref, jint dim) {
+                                                jlong ref, jint dim) {
   af_array ret = 0;
   AF_CHECK(af_mean(&ret, ARRAY(ref), dim));
   return JLONG(ret);
 }
 
-JNIEXPORT jlong JNICALL STATISTICS_FUNC(afMeanWeighted)(JNIEnv *env, jclass clazz,
-                                                        jlong ref, jlong weightsRef,
+JNIEXPORT jlong JNICALL STATISTICS_FUNC(afMeanWeighted)(JNIEnv *env,
+                                                        jclass clazz, jlong ref,
+                                                        jlong weightsRef,
                                                         jint dim) {
   af_array ret = 0;
   AF_CHECK(af_mean_weighted(&ret, ARRAY(ref), ARRAY(weightsRef), dim));
   return JLONG(ret);
 }
 
-JNIEXPORT jdouble JNICALL STATISTICS_FUNC(afMeanAll)(JNIEnv *env,
-                                                    jclass clazz, jlong ref,
-                                                    jintArray dims) {
+JNIEXPORT jdouble JNICALL STATISTICS_FUNC(afMeanAll)(JNIEnv *env, jclass clazz,
+                                                     jlong ref) {
   double ret = 0;
-  jint *dimptr = env->GetIntArrayElements(dims, 0);
-  dim_t tdims[4] = {dimptr[0], dimptr[1], dimptr[2], dimptr[3]};
   AF_CHECK(af_mean_all(&ret, NULL, ARRAY(ref)));
-  env->ReleaseIntArrayElements(dims, dimptr, 0);
-  return  (jdouble)ret;
+  return (jdouble)ret;
 }
 
-#define INSTANTIATE_MEAN(jtype, param)                                  \
-    JNIEXPORT jobject JNICALL STATISTICS_FUNC(afMeanAll##jtype)(JNIEnv *env, jclass clazz, \
-                                                               jlong ref, \
-                                                               jintArray dims) { \
-    double real = 0, img = 0;                                           \
-    jint *dimptr = env->GetIntArrayElements(dims, 0);                   \
-    dim_t tdims[4] = {dimptr[0], dimptr[1], dimptr[2], dimptr[3]};      \
-    AF_CHECK(af_mean_all(&real, &img, ARRAY(ref)));                     \
-    env->ReleaseIntArrayElements(dims, dimptr, 0);                      \
-    jclass cls = env->FindClass("com/arrayfire/"#jtype);                \
-    jmethodID id = env->GetMethodID(cls, "<init>", "("#param")V");      \
-    jobject obj = env->NewObject(cls, id, real, img);                   \
-    return obj;                                                         \
+JNIEXPORT jdouble JNICALL STATISTICS_FUNC(afMeanAllWeighted)(JNIEnv *env,
+                                                             jclass clazz,
+                                                             jlong ref,
+                                                             jlong weightsRef) {
+  double ret = 0;
+  AF_CHECK(af_mean_all_weighted(&ret, NULL, ARRAY(ref), ARRAY(weightsRef)));
+  return (jdouble)ret;
 }
+
+#define INSTANTIATE_MEAN(jtype, param)                               \
+  JNIEXPORT jobject JNICALL STATISTICS_FUNC(afMeanAll##jtype)(       \
+      JNIEnv * env, jclass clazz, jlong ref) {                       \
+    double real = 0, img = 0;                                        \
+    AF_CHECK(af_mean_all(&real, &img, ARRAY(ref)));                  \
+    jclass cls = env->FindClass("com/arrayfire/" #jtype);            \
+    jmethodID id = env->GetMethodID(cls, "<init>", "(" #param ")V"); \
+    jobject obj = env->NewObject(cls, id, real, img);                \
+    return obj;                                                      \
+  }
+
+#define INSTANTIATE_MEAN_WEIGHTED(jtype, param)                            \
+  JNIEXPORT jobject JNICALL STATISTICS_FUNC(afMeanAll##jtype##Weighted)(   \
+      JNIEnv * env, jclass clazz, jlong ref, jlong weightsRef) {           \
+    double real = 0, img = 0;                                              \
+    AF_CHECK(                                                              \
+        af_mean_all_weighted(&real, &img, ARRAY(ref), ARRAY(weightsRef))); \
+    jclass cls = env->FindClass("com/arrayfire/" #jtype);                  \
+    jmethodID id = env->GetMethodID(cls, "<init>", "(" #param ")V");       \
+    jobject obj = env->NewObject(cls, id, real, img);                      \
+    return obj;                                                            \
+  }
 
 INSTANTIATE_MEAN(FloatComplex, FF)
 INSTANTIATE_MEAN(DoubleComplex, DD)
+INSTANTIATE_MEAN_WEIGHTED(FloatComplex, FF)
+INSTANTIATE_MEAN_WEIGHTED(DoubleComplex, DD)
 
 END_EXTERN_C
-
-
